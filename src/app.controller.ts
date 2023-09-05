b33ea3ccf8f149baa71f993d3ed129c60a739acb
Gabriel Omar Berdejo Pareja <gabrielberdejo96@gmail.com>
2023-09-04 23:07:26 -0500

    implement sequelize

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cat, Dog } from '@gberdejo/demo-galicarlib/';
+
+import { Repository } from 'sequelize-typescript';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('CATS_REPOSITORY')
+    private readonly catsRepository: Repository<Cat>,
+    @Inject('DOGS_REPOSITORY')
+    private readonly dogsRepository: Repository<Dog>,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/registerPets')
+  async registerPets(): Promise<Dog[]> {
+    await this.dogsRepository.create({});
+    return await this.dogsRepository.findAll();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UtilsModule, UtilsService } from '@gberdejo/demo-galicarlib';
+import {
+  UtilsModule,
+  UtilsService,
+  databaseModels,
+  catsProviders,
+} from '@gberdejo/demo-galicarlib';
 import { AppService } from './app.service';
+import { Sequelize } from 'sequelize-typescript';
 
 @Module({
   imports: [UtilsModule],
   controllers: [AppController],
-  providers: [AppService, UtilsService],
+  providers: [
+    ...catsProviders,
+    {
+      provide: 'DATABASE_CONNECTION',
+      useFactory: async () => {
+        const sequelize = new Sequelize({
+          dialect: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'mysql',
+          database: 'test',
+        });
+        sequelize.addModels(databaseModels);
+        await sequelize.sync();
+        return sequelize;
+      },
+    },
+    AppService,
+    UtilsService,
+  ],
 })
 export class AppModule {}
